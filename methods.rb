dfedc31f628b00f89f545720cbbd4e53bd933634
damaris <mdamarisnduku@gmail.com>
2022-08-16 10:27:17 +0300

    commit tree ffb52e7498f458c7518f6693030de7fdf877961f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+   
+  end
+
+  def greet(name="Damaris")
+    puts "Hello, #{name}!"
+   
+  end
+greet()
+  def greet_with_default(name="Hello, programmer!")
+    puts "Hello, #{name}!"
+  end
+  greet_with_default()
+
+  def add (num1, num2)
+    return num1+num2
+  end
+
+def halve(num1)
+    if num1.is_a?(Integer)
+     num1/2
+     else
+        nil
+     end
+
